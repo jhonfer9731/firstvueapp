236f83dc5e8c1e15c2ea1a1d2b7db2feac2eaae0
Jhon Benavides <jfernando.benavides@udea.edu.co>
2020-07-06 00:26:41 -0500

    Se agrego la opcion de subir varias imagenes, hay que mejorar el boton edit y la opcion de borrar imagenes en editar

--- src/views/administrador/ProductosAdmin.vue
+++ src/views/administrador/ProductosAdmin.vue
@@ -6,67 +6,209 @@
       <button class="btn btn-primary float-right" @click="addNew">Agregar Producto</button>
     </div>
     <div class="table-responsive">
-    <table class="table">
-      <thead class="thead-dark">
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Nombre</th>
-          <th scope="col">Precio</th>
-          <th scope="col">Imagen</th>
-          <th scope="col">Modificar</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(productoLoop,index) in productos" :key="index" >
-          <td scope="row">
-            <p class="col-lg">
-              {{index+1}}
-            </p>
+      <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col-lg">#</th>
+            <th scope="col-lg">Nombre</th>
+            <th scope="col-lg">Descripción</th>
+            <th scope="col-lg">Precio</th>
+            <th scope="col-lg">Tags</th>
+            <th scope="col-lg">Variantes</th>
+            <th scope="col-lg">Imagen</th>
+            <th scope="col-lg">Modificar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(productoLoop,index) in productos" :key="index">
+            <td scope="row">
+              <p class="col-lg">{{index+1}}</p>
             </td>
-          <td scope="row">
-            <p class="edit-off col-lg" v-show="!productoLoop.editOn">{{productoLoop.data().nombre}}</p>
-            <div class="edit-on col-lg" v-show="productoLoop.editOn">
-              <input type="text" class="form-control" v-model="producto.nombre" />
-            </div>
-          </td>
-          <td scope="row">
-            <p class="edit-off col-lg" v-show="!productoLoop.editOn">{{productoLoop.data().precio}}</p>
-            <div class="edit-on col-lg" v-show="productoLoop.editOn">
-              <input type="text" class="form-control" v-model="producto.precio" />
-            </div>
-          </td>
-          <td scope="row">
-            <div class="col-lg">
-              <div class="">
-                <img
-                  :src="productoLoop.data().image"
-                  :alt="eone = 'no existe imagen'"
-                  class=" producto-imagen"
-                />
-              </div>
-            </div>
-          </td>
-          <td scope="row">
-            <div class="col-lg">
-              <button
-                @click="editarProducto(index)"
-                class="btn mx-2"
-                :class="{'btn-primary': !productoLoop.editOn}"
+            <td scope="row">
+              <p
+                class="edit-off col-lg"
                 v-show="!productoLoop.editOn"
-                :disabled="isDisabled(index)"
-              >Editar</button>
-              <button
-                @click="updateData(index)"
-                class="btn mx-2"
-                :class="{'btn-warning': productoLoop.editOn}"
-                v-show="productoLoop.editOn"
-              >Guardar</button>
-              <button @click="borrarProducto(productoLoop.id)" class="btn btn-danger">Borrar</button>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+              >{{productoLoop.data().nombre}}</p>
+              <div class="edit-on col-lg" v-show="productoLoop.editOn">
+                <input type="text" class="form-control" v-model="producto.nombre" />
+              </div>
+            </td>
+            <td scope="row">
+              <!--
+              <div :id="'accordion'+index">
+                <div class="card">
+                  <div class id="headingDescripcion">
+                    <h5 class="mb-0">
+                      <button
+                        class="btn btn-link"
+                        data-toggle="collapse"
+                        :data-target="'#'+index+'-acordeon'"
+                        aria-expanded="true"
+                        :aria-controls="index+'-acordeon'"
+                      >Ver Detalles</button>
+                    </h5>
+                  </div>
+                  <div
+                    :id="index+'-acordeon'"
+                    class="collapse show"
+                    aria-labelledby="headingDescripcion"
+                    :data-parent="'accordion'+index"
+                  >
+                    <div class="card-body">
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+              -->
+              <div :id="'accordion-'+index">
+                <div class="card">
+                  <div class="card-header" :id="'heading'+index">
+                    <h5 class="mb-0">
+                      <button
+                        class="btn btn-link"
+                        data-toggle="collapse"
+                        :data-target="'#collapse'+index"
+                        aria-expanded="false"
+                        aria-controls="collapseOne"
+                      >Ver Detalles</button>
+                    </h5>
+                  </div>
+
+                  <div
+                    :id="'collapse'+index"
+                    class="collapse"
+                    :aria-labelledby="'heading'+index"
+                    :data-parent="'#'+'accordion-'+index"
+                  >
+                    <div class="card-body">
+                      <p
+                        class="edit-off col-lg"
+                        v-show="!productoLoop.editOn"
+                        v-html="productoLoop.data().descripcion"
+                      ></p>
+                      <div class="edit-on col-lg" v-show="productoLoop.editOn">
+                        <VueEditor v-model="producto.descripcion" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td scope="row">
+              <p
+                class="edit-off col-lg"
+                v-show="!productoLoop.editOn"
+              >{{productoLoop.data().precio}}</p>
+              <div class="edit-on col-lg" v-show="productoLoop.editOn">
+                <input type="text" class="form-control" v-model="producto.precio" />
+              </div>
+            </td>
+            <td scope="row">
+              <div
+                class="show-tags edit-off col-lg"
+                v-if="productoLoop.data().tags"
+                v-show="!productoLoop.editOn"
+              >
+                <li
+                  v-for="(tagProducto,index) in productoLoop.data().tags"
+                  :key="index"
+                  class="d-inline-block m-2 border px-1"
+                >{{tagProducto}}</li>
+              </div>
+              <div class="edit-on col-lg" v-show="productoLoop.editOn">
+                <div class="form-group">
+                  <input
+                    class="form-control"
+                    type="text"
+                    placeholder="Tags"
+                    v-model="tag"
+                    @keyup.188="addTag"
+                  />
+                  <div class="show-tags" v-if="producto.tags"><!-- se elimino .length -->
+                    <li
+                      v-for="(tagProducto,index) in producto.tags"
+                      :key="index"
+                      class="d-inline-block m-2 border px-1"
+                    >
+                      {{tagProducto}}
+                      <a href="#" @click="removeTag(index)">
+                        <i class="fa fa-times-circle" aria-hidden="true"></i>
+                      </a>
+                    </li>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td scope="row">
+              <div
+                class="show-tags edit-off col-lg"
+                v-if="productoLoop.data().variantes"
+                v-show="!productoLoop.editOn"
+              >
+                <!-- tiene que ser sin el length ya que algunos no tienen el parametro variantes -->
+                <li
+                  v-for="(varianteProducto,index) in productoLoop.data().variantes"
+                  :key="index"
+                  class="d-inline-block m-2 border px-1"
+                >{{varianteProducto}}</li>
+              </div>
+              <div class="edit-on col-lg" v-show="productoLoop.editOn">
+                <div class="form-group">
+                  <input
+                    class="form-control"
+                    type="text"
+                    placeholder="Variantes"
+                    v-model="variante"
+                    @keyup.188="addVariante"
+                  />
+                  <div class="show-tags" v-if="producto.variantes"> <!-- se elimino el .length -->
+                    <li
+                      v-for="(varianteProducto,index) in producto.variantes"
+                      :key="index"
+                      class="d-inline-block m-2 border px-1"
+                    >
+                      {{varianteProducto}}
+                      <a href="#" @click="removeVariante(index)">
+                        <i class="fa fa-times-circle" aria-hidden="true"></i>
+                      </a>
+                    </li>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td scope="row">
+              <div class="col-lg">
+                <div class>
+                  <img
+                    :src="productoLoop.data().image"
+                    :alt="alternative = 'no existe imagen'"
+                    class="producto-imagen"
+                  />
+                </div>
+              </div>
+            </td>
+            <td scope="row">
+              <div class="col-lg">
+                <button
+                  @click="editarProducto(index)"
+                  class="btn mx-2"
+                  :class="{'btn-primary': !productoLoop.editOn}"
+                  v-show="!productoLoop.editOn"
+                  :disabled="isDisabled(index)"
+                >Editar</button>
+                <button
+                  @click="updateData(index)"
+                  class="btn mx-2"
+                  :class="{'btn-warning': productoLoop.editOn}"
+                  v-show="productoLoop.editOn"
+                >Guardar</button>
+                <button @click="borrarProducto(productoLoop.id)" class="btn btn-danger">Borrar</button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
     <!-- Modal -->
     <div
@@ -121,7 +263,7 @@
                       v-model="tag"
                       @keyup.188="addTag"
                     />
-                    <div class="show-tags" v-if="producto.tags.length">
+                    <div class="show-tags" v-if="producto.tags"> <!-- se elimino el .length -->
                       <li
                         v-for="(tagProducto,index) in producto.tags"
                         :key="index"
@@ -135,8 +277,49 @@
                     </div>
                   </div>
                   <div class="form-group">
+                    <input
+                      class="form-control"
+                      type="text"
+                      placeholder="Variantes"
+                      v-model="variante"
+                      @keyup.188="addVariante"
+                    />
+                    <div class="show-tags" v-if="producto.variantes"> <!-- Se elimino el length -->
+                      <li
+                        v-for="(varianteProducto,index) in producto.variantes"
+                        :key="index"
+                        class="d-inline-block m-2 border px-1"
+                      >
+                        {{varianteProducto}}
+                        <a href="#" @click="removeVariante(index)">
+                          <i class="fa fa-times-circle" aria-hidden="true"></i>
+                        </a>
+                      </li>
+                    </div>
+                  </div>
+                  <div class="form-group">
                     <label for="product_image">Imagenes Producto</label>
                     <input type="file" @change="cargarImagen" class="form-control" />
+                    <div class="progress my-2" v-show="progresoCargaImg < 100 && progresoCargaImg >0">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        :style="'width: '+progresoCargaImg+'%'"
+                        :aria-valuenow="progresoCargaImg"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      ><span v-if="progresoCargaImg < 100 && progresoCargaImg > 0">Cargando Imagen...</span>
+                      </div>
+                      
+                    </div>
+                  </div>
+                  <div class="form-group d-flex">
+                    <div v-for="(imagen,index) in producto.images" :key="index" class="p-1">
+                      <div class="img-wrapp">
+                        <img :src="imagen" alt="Error" width="80px" />
+                        <span class="delete-img" @click="deleteImage(imagen,index)">X</span>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -168,38 +351,72 @@ export default {
         precio: null,
         descripcion: null,
         tags: [],
-        image: null
+        image: null,
+        images: [],
+        variantes: []
       },
-      //products: [],
-      //editOn: false,
-      activeItemIndex: null,
+      activeItemIndex: null /*Se selecciona un item para editar*/,
       activeItemId: null,
-      tag: ""
+      tag: "",
+      variante: "",
+      progresoCargaImg: 0
     };
   },
   methods: {
+    deleteImage(img, index) {
+      let image = fb.storage().refFromURL(img); //Se obtiene la referencia de la img como un objeto de donde esta ubicada en firebase
+      this.producto.images.splice(index, 1);
+      image
+        .delete()
+        .then(() => {
+          console.log("Imagen borrada");
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
     cargarImagen(e) {
-      let file = e.target.files[0];
-      var storageRef = fb.storage().ref("products/" + file.name);
-      let uploadTask = storageRef.put(file); // para poder ultizar los metodos de task
-      uploadTask.on(
-        "state_changed",
-        snapshot => {
-          console.log(snapshot);
-        },
-        error => {
-          console.log(error);
-        },
-        () => {
-          //successfull part, it runs when the load is completed
-          uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
-            this.producto.image = downloadURL;
-          });
-        }
-      );
+      if (e.target.files[0]) {
+        const d = new Date();
+        let file = e.target.files[0];
+        var storageRef = fb
+          .storage()
+          .ref("products/" + file.name + "_" + d.toISOString());
+        let uploadTask = storageRef.put(file); // para poder ultizar los metodos de task
+        uploadTask.on(
+          "state_changed",
+          snapshot => {
+            console.log(snapshot);
+            this.progresoCargaImg = snapshot.bytesTransferred*100/snapshot.totalBytes
+            console.log(this.progresoCargaImg)
+          },
+          error => {
+            console.log(error);
+          },
+          () => {
+            //successfull part, it runs when the load is completed
+            uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
+              this.producto.image = downloadURL;
+              this.producto.images.push(downloadURL);
+            });
+          }
+        );
+      }
       //console.log(e.target.files[0])
     },
+    reset(){
+      this.producto = {
+        nombre: null,
+        precio: null,
+        descripcion: null,
+        tags: [''],
+        image: null,
+        images: [],
+        variantes: ['']
+      }
+    },
     addNew() {
+      this.reset()
       window.$("#NuevoProducto").modal("show");
     },
     agregarProducto() {
@@ -304,6 +521,15 @@ export default {
     },
     removeTag(index) {
       this.producto.tags.splice(index, 1);
+    },
+    addVariante() {
+      this.producto.variantes.push(
+        this.variante.substring(0, this.variante.length - 1)
+      );
+      this.variante = "";
+    },
+    removeVariante(index) {
+      this.producto.variantes.splice(index, 1);
     }
   },
   created() {
@@ -335,6 +561,21 @@ export default {
 .producto-imagen {
   /*width: 80%;*/
   min-width: 150px;
-  max-height:200px;
+  max-height: 150px;
+}
+
+.img-wrapp {
+  position: relative;
+}
+
+.img-wrapp span.delete-img {
+  position: absolute;
+  top: -14px;
+  left: -2px;
+}
+
+.img-wrapp span.delete-img:hover {
+  cursor: pointer;
+  font-weight: bold;
 }
 </style>
